b3575e59767684ee965a1c9d09ff5af913af8fcc
Raghad Islam <raghad.noser.2003@gmail.com>
2025-11-01 01:14:52 +0200

    feat: load questions from fake api, handle error and ready status, and add startScreen component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+
+const initialState = {
+  questions: [],
+  status: "loading", // 'loading', 'ready', 'active', 'finished', 'error'
+  // currentQuestionIndex: 0,
+  // score: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error(`Unknown action type: ${action.type}`);
+  }
+}
+
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((error) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Questiuon?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
---
+++ src/StartScreen.js
@@ -0,0 +1,11 @@
+function StartScreen({ numQuestions }) {
+  return (
+    <div className="start">
+      <h2>Welcome to The React Quiz</h2>
+      <h3>{numQuestions} questions to test your React mastery</h3>
+      <button className="btn btn-ui">Start Quiz</button>
+    </div>
+  );
+}
+
+export default StartScreen;
